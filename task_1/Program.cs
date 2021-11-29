fc157679834dd7a5b40d45ba1b6c4a187d325ef7
Sharisi123 <nasir1231011@gmail.com>
2021-11-29 11:40:44 +0200

    practic 3

---
+++ task_1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+
+namespace task_1
+{
+  class Program
+  {
+    class BankAccount
+    {
+      public static int id = 0;
+
+      private int Balance { get; set; }
+      private string Currency { get; set; }
+      public BankAccount(int balance = 0, string currency = "uan")
+      {
+        id++;
+        Balance = balance;
+        Currency = currency;
+      }
+
+      public void Deposit(int amount)
+      {
+        if (Currency == "uan" & amount > 50000 | Currency == "usd" & amount > 2000 | Currency == "eu" & amount > 2000)
+          Console.WriteLine("We will notify a tax office");
+
+        Balance += amount;
+        Console.WriteLine($"Depositing {amount}, current balance is {Balance} \n");
+      }
+
+      public void Withdraw(int amount)
+      {
+        if (amount > Balance)
+        {
+          Console.WriteLine($"Not enough money, your current balance is: {Balance} \n");
+          return;
+        }
+        Balance -= amount;
+        Console.WriteLine($"Withdrawing {amount}, current balance is {Balance} \n");
+      }
+
+      public void ShowInfo()
+      {
+        Console.WriteLine($"Number of balance is: {id} \nCurrent balance is: {Balance} {Currency} \n");
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      PrintTimeStamp("Наживотов Олександр");
+
+      static void PrintTimeStamp(string name)
+      {
+        Console.OutputEncoding = Encoding.UTF8;
+        DateTime localDate = DateTime.Now;
+        var culture = new CultureInfo("ru-RU");
+
+        Console.WriteLine("\nДата та час компіляції: {0}", localDate.ToString(culture));
+        Console.WriteLine("Автор: {0}", name);
+      }
+
+      BankAccount account = new BankAccount();
+      account.Deposit(300);
+      account.Withdraw(301);
+      account.ShowInfo();
+    }
+  }
+}
---
+++ task_2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text;
+using System.Globalization;
+
+namespace task_2
+{
+  class Program
+  {
+    static public int Cheaker(int value)
+    {
+      if (value <= 0)
+      {
+        Console.WriteLine($"Setter change {value} on 1");
+        return 1;
+      }
+      return value;
+
+    }
+    class Rectangle
+    {
+      private int _w;
+      private int _l;
+      private int _h;
+      public int W
+      {
+        get
+        {
+          return _w;
+        }
+        private set
+        {
+          _w = Cheaker(value);
+        }
+      }
+      public int L
+      {
+        get
+        {
+          return _l;
+        }
+        private set
+        {
+          _l = Cheaker(value);
+        }
+      }
+      public int H
+      {
+        get
+        {
+          return _h;
+        }
+        private set
+        {
+          _h = Cheaker(value);
+        }
+      }
+      public Rectangle(int w, int l, int h)
+      {
+        W = w;
+        L = l;
+        H = h;
+      }
+
+      int findVolume()
+      {
+        return L * W * H;
+      }
+      int findLateralSurfaceArea()
+      {
+        return 2 * L * H + 2 * W * H;
+      }
+
+      int findSurfaceArea()
+      {
+        return 2 * L * W + 2 * L * H + 2 * W * H;
+      }
+    }
+    static void Main(string[] args)
+    {
+      PrintTimeStamp("Наживотов Олександр");
+
+      static void PrintTimeStamp(string name)
+      {
+        Console.OutputEncoding = Encoding.UTF8;
+        DateTime localDate = DateTime.Now;
+        var culture = new CultureInfo("ru-RU");
+
+        Console.WriteLine("\nДата та час компіляції: {0}", localDate.ToString(culture));
+        Console.WriteLine("Автор: {0}", name);
+      }
+
+      Rectangle rect = new Rectangle(5, 2, 4);
+      Rectangle rect1 = new Rectangle(-5, 2, 4);
+    }
+  }
+}
